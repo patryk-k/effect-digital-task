10492aef4edf07460e33481b3a24bd622c2ec9b2
patryk-k <patryk-k@outlook.com>
2024-02-07 19:08:56 +0000

    refactor unit test

--- tests/Unit/DocumentFactoryTest.php
+++ tests/Unit/DocumentFactoryTest.php
@@ -14,10 +14,7 @@ class DocumentFactoryTest extends TestCase
     {
         $model = Document::factory()->create();
 
-        $this->assertTrue(is_a($model, Document::class));
-        $this->assertTrue($model->exists);
-        $this->assertNotEmpty($model->request_time);
-        $this->assertNotEmpty($model->text);
+        $this->assertModelIsValid($model);
     }
 
     /**
@@ -28,10 +25,15 @@ class DocumentFactoryTest extends TestCase
         $models = Document::factory()->count(3)->create();
 
         foreach ($models as $_model) {
-            $this->assertTrue(is_a($_model, Document::class));
-            $this->assertTrue($_model->exists);
-            $this->assertNotEmpty($_model->request_time);
-            $this->assertNotEmpty($_model->text);
+            $this->assertModelIsValid($_model);
         }
+    }
+
+    private function assertModelIsValid($model): void
+    {
+        $this->assertTrue(is_a($model, Document::class));
+        $this->assertTrue($model->exists);
+        $this->assertNotEmpty($model->request_time);
+        $this->assertNotEmpty($model->text);
     }
 }
