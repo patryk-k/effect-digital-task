48f437453f56a2fe0ba431faed81422278321767
patryk-k <patryk-k@outlook.com>
2024-02-07 19:30:15 +0000

    model

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -5,7 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property-read int|null $id
+ * @property string|null $text
+ * @property Carbon|null $request_time
+ * @property-read Carbon|null $created_at
+ * @property-read Carbon|null $updated_at
+ */
 class Document extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'text',
+        'request_time',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'request_time' => 'datetime',
+    ];
 }
